c8ff32ed5ac165bfdd5459a6df42681a9669dda7
gustav0leal <gustavoleal@discente.ufg.br>
2023-02-26 20:49:56 -0300

    update

--- app/src/main/java/com/example/thermoledmobileclient/FullAccess.java
+++
@@ -1,4 +0,0 @@
-package com.example.thermoledmobileclient;
-
-public class FullAccess {
-}
--- app/src/main/java/com/example/thermoledmobileclient/MainActivity.java
+++ app/src/main/java/com/example/thermoledmobileclient/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.thermoledmobileclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
 import android.content.Context;
@@ -13,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.thermoledmobileclient.data.model.LoggedInUser;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.ref.WeakReference;
@@ -21,27 +27,38 @@ import java.util.concurrent.TimeUnit;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.examples.iotservice.IoTServiceGrpc;
-import io.grpc.examples.iotservice.LedMessage;
+import io.grpc.examples.iotservice.LedReply;
+import io.grpc.examples.iotservice.LedRequest;
 import io.grpc.examples.iotservice.TemperatureReply;
 import io.grpc.examples.iotservice.TemperatureRequest;
 
+
+import com.example.thermoledmobileclient.ui.login.LoginViewModel;
+import com.example.thermoledmobileclient.ui.login.LoginViewModelFactory;
 public class MainActivity extends AppCompatActivity {
     private TextView temperatureResultText;
-    private EditText hostEdit;
-    private EditText portEdit;
+    private String host;
+    private int port;
+    private int key;
     private Button getTemperatureButton;
     private Button ledOnButton;
     private Button ledOffButton;
 
+    private LoggedInUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LoginViewModel vm = new ViewModelProvider(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+        user = vm.getLoggedInUser();
         temperatureResultText = (TextView) findViewById(R.id.textViewTemperature);
-        hostEdit = (EditText) findViewById(R.id.host);
-        portEdit = (EditText) findViewById(R.id.port);
+        host = user.getUserHost();
+        port = user.getUserPort();
+        key = user.getUserId();
+//        hostEdit = (EditText) findViewById(R.id.host);
+//        portEdit = (EditText) findViewById(R.id.port);
         getTemperatureButton = (Button) findViewById(R.id.getTempButton);
         ledOnButton = (Button) findViewById(R.id.ledOnButton);
         ledOffButton = (Button) findViewById(R.id.ledOffButton);
@@ -49,11 +66,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendTemperatureRequest(View view) {
-        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
-                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+//        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+//                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
         getTemperatureButton.setEnabled(false);
         temperatureResultText.setText("");
-        new GrpcTask(this).execute(hostEdit.getText().toString(), portEdit.getText().toString(), "sensor01");
+        Integer iport = new Integer(port);
+        Integer ikey = new Integer(key);
+        new GrpcTask(this).execute(host,iport.toString(), ikey.toString());
     }
 
     private static class GrpcTask extends AsyncTask<String, Void, String> {
@@ -70,14 +89,18 @@ public class MainActivity extends AppCompatActivity {
 
             String host = params[0];
             String portStr = params[1];
-            String sensorName = params[2];
+            String keyStr = params[2];
             int port = TextUtils.isEmpty(portStr) ? 0 : Integer.parseInt(portStr);
+            int key = TextUtils.isEmpty(keyStr) ? 0 : Integer.parseInt(keyStr);
+
             try {
                 channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
                 IoTServiceGrpc.IoTServiceBlockingStub stub = IoTServiceGrpc.newBlockingStub(channel);
-                TemperatureRequest request = TemperatureRequest.newBuilder().setSensorName(sensorName).build();
+                TemperatureRequest request = TemperatureRequest.newBuilder().setKey(key).build();
                 TemperatureReply reply = stub.sayTemperature(request);
-                return reply.getTemperature();
+                if(reply.getStatus().equals("OK"))
+                    return reply.getTemperature();
+                throw new Exception(reply.getStatus());
             } catch (Exception e) {
                 StringWriter sw = new StringWriter();
                 PrintWriter pw = new PrintWriter(sw);
@@ -106,17 +129,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ledOnRequest(View view) {
-        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
-                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+//        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+//                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
         ledOnButton.setEnabled(false);
-        new GrpcTask2(this).execute(hostEdit.getText().toString(), portEdit.getText().toString(), "1");
+        Integer iport = new Integer(port);
+        Integer ikey = new Integer(key);
+        new GrpcTask2(this).execute(host,iport.toString(), ikey.toString(),"1");
     }
 
     public void ledOffRequest(View view) {
-        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
-                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
+//        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+//                .hideSoftInputFromWindow(hostEdit.getWindowToken(), 0);
         ledOffButton.setEnabled(false);
-        new GrpcTask2(this).execute(hostEdit.getText().toString(), portEdit.getText().toString(), "0");
+
+        Integer iport = new Integer(port);
+        Integer ikey = new Integer(key);
+        new GrpcTask2(this).execute(host,iport.toString(), ikey.toString(),"0");
     }
 
     private static class GrpcTask2 extends AsyncTask<String, Void, String> {
@@ -133,14 +161,16 @@ public class MainActivity extends AppCompatActivity {
 
             String host = params[0];
             String portStr = params[1];
-            String ledState = params[2];
+            String keyStr = params[2];
             int port = TextUtils.isEmpty(portStr) ? 0 : Integer.parseInt(portStr);
+            int key = TextUtils.isEmpty(keyStr) ? 0 : Integer.parseInt(keyStr);
+            String ledState = params[3];
             try {
                 channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
                 IoTServiceGrpc.IoTServiceBlockingStub stub = IoTServiceGrpc.newBlockingStub(channel);
-                LedMessage request = LedMessage.newBuilder().setState(Integer.parseInt(ledState)).build();
-                LedMessage reply = stub.blinkLed(request);
-                return Integer.toString(reply.getState());
+                LedRequest request = LedRequest.newBuilder().setLedname("red").setKey(key).setState(Integer.parseInt(ledState)).build();
+                LedReply reply = stub.blinkLed(request);
+                return Integer.toString(reply.getLedstateMap().get("red"));
             } catch (Exception e) {
                 StringWriter sw = new StringWriter();
                 PrintWriter pw = new PrintWriter(sw);
